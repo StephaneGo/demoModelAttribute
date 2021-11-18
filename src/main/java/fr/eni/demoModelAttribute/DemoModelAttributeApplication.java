65fc21185bebbbdd48eaa3c6b2657e764dc302dc
sgobin <sgobin@21SE43-1S0TVD3.ad.campus-eni.fr>
2021-11-18 17:11:00 +0100

    chgt1

--- src/main/java/fr/eni/demoModelAttribute/DemoModelAttributeApplication.java
+++ src/main/java/fr/eni/demoModelAttribute/DemoModelAttributeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoModelAttributeApplication {
 
+	//chgt1
 	public static void main(String[] args) {
 		SpringApplication.run(DemoModelAttributeApplication.class, args);
 	}
