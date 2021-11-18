983a559bfb33a8d3b3e0c7661ded225e7ce95ea1
sgobin <sgobin@21SE43-1S0TVD3.ad.campus-eni.fr>
2021-11-18 17:01:24 +0100

    commit 2

--- src/main/java/fr/eni/demoModelAttribute/controllers/DemoModelAttributeController.java
+++ src/main/java/fr/eni/demoModelAttribute/controllers/DemoModelAttributeController.java
@@ -14,7 +14,6 @@ import fr.eni.demoModelAttribute.model.Compteur;
 @RequestMapping({"/", "/demoModelAttribute"})
 @SessionAttributes("compteur")
 public class DemoModelAttributeController {
-	//changement
 	//initialisation du compteur
 	@ModelAttribute("compteur") 
 	public Compteur getCompteur() {
